8bb5fc740ac8a60f0d47b0e16455820cc575603f
Anton Kargapolov <igloooooook@gmail.com>
2023-02-24 14:01:12 +0500

    task 045

---
+++ task_045/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 45: Напишите программу, 
+// которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[ " + string.Join(", ", array) + "]");
+}
+
+int[] CopyPaste(int[] array)
+{
+    int[] array2 = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array2[i]=array[i];
+    }
+    return array2;
+}
+
+int[] arr = GenerateArray(10,0,10);
+PrintArray(arr);
+int[] arrcopy= CopyPaste(arr);
+PrintArray(arrcopy);
