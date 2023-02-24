af4f4da01d0ef6d5bbd2f21c79d27637edf286d6
Anton Kargapolov <igloooooook@gmail.com>
2023-02-24 13:15:06 +0500

    task 042

---
+++ task_042/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+// System.Console.WriteLine("ВВедите число ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// string st = "";
+
+// while (n != 0)
+// {
+//     st += n % 2;
+//     n = n / 2;
+// }
+
+// for (int i = st.Length - 1; i >= 0; i--)
+// {
+//     System.Console.Write(st[i]);
+// }
+// System.Console.WriteLine( );
+
+// Решение 2
+void GetBinatyView(int number)
+{
+    if(number<=0) return;
+    GetBinatyView(number/2);
+    System.Console.Write(number%2);
+}
+
+
+System.Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+GetBinatyView(N);
