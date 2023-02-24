0b95303d7cf49a38194c3c6e114e1f8523b30c0b
Anton Kargapolov <igloooooook@gmail.com>
2023-02-24 11:35:32 +0500

    task 040

---
+++ task_040/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет,
+//  может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+metka:
+System.Console.WriteLine("Введите числа: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+if(array.Length!=3)
+{
+    System.Console.WriteLine("Массив введен не корректно ");
+    goto metka;
+}
+
+if((array[0]+array[1])>array[2] && (array[1]+array[2])>array[0] && (array[2]+array[0])>array[1])
+{
+    System.Console.WriteLine("Такой треугольник существует ");
+}
+else
+{
+    System.Console.WriteLine("Такого треугольника не существует");
+}
